a3b704e192ed3783d596e03a1aba53c19c815b88
Shoshin <m.yanagihara@gmail.com>
2020-09-14 18:11:06 -1000

    Fixing package.json

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -1,3 +1,5 @@
 /* after changing this file run 'npm run webpack:build' */
 import '../content/scss/vendor.scss';
+
+// Import Azure Maps source from node_modules
 import 'azure-maps-control/dist/atlas.min.js';
