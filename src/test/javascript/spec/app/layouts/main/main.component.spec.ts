a89e33789dc7699bdd96e4c6bb8c5ce526e74e5a
Shoshin <m.yanagihara@gmail.com>
2020-09-14 21:26:54 -1000

    Fixed unit test

--- src/test/javascript/spec/app/layouts/main/main.component.spec.ts
+++ src/test/javascript/spec/app/layouts/main/main.component.spec.ts
@@ -35,7 +35,7 @@ describe('Component Tests', () => {
 
     describe('page title', () => {
       let routerState: any;
-      const defaultPageTitle = 'Markcop';
+      const defaultPageTitle = 'MARKCOP';
       const parentRoutePageTitle = 'parentTitle';
       const childRoutePageTitle = 'childTitle';
       const navigationEnd = new NavigationEnd(1, '', '');
