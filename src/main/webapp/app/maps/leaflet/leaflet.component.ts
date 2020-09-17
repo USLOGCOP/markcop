85ddf00fa90d1c677805e23395cac368b6fdab45
Shoshin <m.yanagihara@gmail.com>
2020-09-17 13:31:40 -1000

    feat: adding additional markers to leaflet map

--- src/main/webapp/app/maps/leaflet/leaflet.component.ts
+++ src/main/webapp/app/maps/leaflet/leaflet.component.ts
@@ -7,27 +7,71 @@ import { latLng, Map, MapOptions, tileLayer, Marker, icon } from 'leaflet';
   styleUrls: ['./leaflet.component.scss'],
 })
 export class LeafComponent implements OnInit {
+  // Reference to the main map component
   private map: any;
 
-  optionsSpec: any = {
-    layers: [{ url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', attribution: 'Open Streets Map' }],
-    zoom: 11,
-    center: [43.296174, 5.369953],
+  // Define our base layers so we can reference them multiple times
+  streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  });
+  wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  });
+
+  // Marker for the top of Mt. Ranier
+  summit = new Marker([46.8523, -121.7603], {
+    icon: icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'assets/marker-icon.png',
+      shadowUrl: 'assets/marker-shadow.png',
+    }),
+  });
+
+  // Marker for the parking lot at the base of Mt. Ranier trails
+  paradise = new Marker([46.78465227596462, -121.74141269177198], {
+    icon: icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'assets/marker-icon.png',
+      iconRetinaUrl: 'assets/marker-icon-2x.png',
+      shadowUrl: 'assets/marker-shadow.png',
+    }),
+  });
+
+  // Layers control object with our two base layers and the three overlay layers
+  layersControl = {
+    baseLayers: {
+      'Street Maps': this.streetMaps,
+      'Wikimedia Maps': this.wMaps,
+    },
+    overlays: {
+      'Mt. Rainier Summit': this.summit,
+      'Mt. Rainier Paradise Start': this.paradise,
+    },
+    position: 'topLeft',
   };
 
-  zoom = this.optionsSpec.zoom;
-  center = latLng(this.optionsSpec.center);
+  // Define initial default map options
+  defaultMapOptions: any = {
+    layers: [this.streetMaps, this.summit, this.paradise],
+    zoom: 7,
+    center: latLng([46.879966, -121.726909]),
+  };
 
+  // Apply initial default map options
   leafletOptions: MapOptions = {
-    layers: [tileLayer(this.optionsSpec.layers[0].url, { attribution: this.optionsSpec.layers[0].attribution })],
-    zoom: this.optionsSpec.zoom,
-    center: latLng(this.optionsSpec.center),
+    layers: this.defaultMapOptions.layers,
+    zoom: this.defaultMapOptions.zoom,
+    center: this.defaultMapOptions.center,
   };
 
   constructor() {}
 
   ngOnInit(): void {
-    this.initializeMapOptions();
+    // this.initializeMapOptions();
   }
 
   onMapReady(map: Map): void {
@@ -49,13 +93,13 @@ export class LeafComponent implements OnInit {
   }
 
   private addSampleMarker(): void {
-    const marker = new Marker([51.51, 0]).setIcon(
+    const newMarker = new Marker([51.51, 0]).setIcon(
       icon({
         iconSize: [25, 41],
         iconAnchor: [13, 41],
         iconUrl: 'assets/marker-icon.png',
       })
     );
-    marker.addTo(this.map);
+    newMarker.addTo(this.map);
   }
 }
